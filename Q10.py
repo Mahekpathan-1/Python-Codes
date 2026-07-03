8bc4e85355a1f9b9b7c22f77284d4221442f7878
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-03 18:38:25 +0530

    python example

---
+++ Q10.py
@@ -0,0 +1,11 @@
+def Square(num):
+    num = num * num
+    print("Sqauare of number is :", num)
+    
+def main():
+    value = (int(input("Enter number:")))
+    Square(value)
+
+if __name__== "__main__":
+    main()
+    
