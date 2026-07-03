4f542cd9fd047b20c6da75cee8aff99990b501cc
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-03 18:51:34 +0530

    python practice

---
+++ Q27.py
@@ -0,0 +1,12 @@
+def DisplayReverse(No):
+    for i in range(No,0 , -1):
+        print(i)
+        
+def main():
+    
+    num= int(input("Enter number : "))
+    DisplayReverse(num)
+    
+if __name__ == "__main__":
+    main()
+        
