ccf870cc8e56061bb42353a265b75c0ca57cc7c0
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-03 18:51:01 +0530

    python practice

---
+++ Q26.py
@@ -0,0 +1,12 @@
+def Display(No):
+    for i in range(1, No+1):
+        print(i)
+        
+def main():
+    
+    num = int(input("Enter A Number : "))
+    
+    Display(num)
+    
+if __name__ == "__main__":
+    main()
