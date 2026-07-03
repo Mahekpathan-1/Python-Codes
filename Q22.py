3994e27f735388cfae37d2327260038af833ee78
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-03 18:45:25 +0530

    python practice

---
+++ Q22.py
@@ -0,0 +1,19 @@
+def CheckPrime(No):
+    if No <= 1:
+        print("Not Prime")
+        return
+
+    for i in range(2, No):
+        if No % i == 0:
+            print("Not Prime")
+            return
+
+    print("Prime Number")
+
+
+def main():
+    Num = int(input("Enter Number : "))
+    CheckPrime(Num)
+
+if __name__ == "__main__":
+    main()
