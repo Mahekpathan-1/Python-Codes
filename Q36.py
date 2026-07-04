cfb00c1df03f186745dc4459fd7a0d02ee24e4bc
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-04 23:59:04 +0530

    lambda function

---
+++ Q36.py
@@ -0,0 +1,13 @@
+Minimum = lambda No1 , No2 : No1 if No1 < No2 else No2
+
+def main():
+    
+    value1 = int(input ("Enter first number : "))
+    value2 = int(input("Enter second Number : "))
+    
+    Ret = Minimum(value1, value2)
+    
+    print("Minimum number is :", Ret)
+    
+if __name__ == "__main__":
+    main()
