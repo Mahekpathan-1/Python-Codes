7f6030f7671945be06972dcaae04daae97278d9c
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-04 23:58:11 +0530

    lambda function

---
+++ Q34.py
@@ -0,0 +1,9 @@
+Cube = lambda No : No * No * No
+
+def main():
+     No = int(input("Enter a number :"))
+     Ret = Cube(No)
+     print("Cube of number is : ", Ret)
+     
+if __name__ == "__main__":
+    main()
