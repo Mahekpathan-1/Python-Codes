202c401ee4df8508a427d345d601a7d8ce4304b9
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-07-08 23:26:44 +0530

    python practice

---
+++ Assignment20_2.py
@@ -0,0 +1,37 @@
+import threading
+
+def EvenFactor(No):
+    
+    Sum = 0
+    
+    print("Even Factor are :")
+    for i in range(2,No+1):
+        if No % i ==0 and i % 2==0:
+            print(i)
+            Sum = Sum + i
+    print("Sum of Even Factor :", Sum)
+    
+def oddFactor(No):
+    sum = 0
+    
+    print("Odd factor are :")
+    for i in range(1,No+1):
+        if No % i == 0 and i % 2 !=0:
+            print(i)
+            sum = sum + i
+    print("Sum of Odd Factor :",sum)
+    
+def main():
+    value = int(input("Enter number :"))
+    
+    t1 = threading.Thread(target = EvenFactor, args=(value,))
+    t2 = threading .Thread(target = oddFactor, args=(value,))
+    
+    t1.start()
+    t2.start()
+    
+    print("Exit from main ")
+    
+    
+if __name__ == "__main__":
+    main()
