042d1c9d3cbb9401ae9dd0e3de71c8a5551bf868
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-08-11 12:45:45 +0530

    Python supervised ML

---
+++ Assignment39_4.py
@@ -0,0 +1,43 @@
+# Confusion matrix using sklearn 
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+    
+    df = pd.read_csv('student_performance_ml.csv')
+    
+    X = df.drop("FinalResult", axis = 1)
+    Y = df["FinalResult"]
+    
+    X_train, X_test, Y_train , Y_test = train_test_split(X, Y, test_size= 0.2, random_state=42)
+    
+    model = DecisionTreeClassifier()
+    
+    model = model.fit(X_train,Y_train)
+    
+    Y_pred = model.predict(X_test)
+    
+    accuracy = accuracy_score(Y_test, Y_pred)
+    
+    print("Decision Tree Accuracy is :", accuracy * 100 , "%")
+    
+    cm = confusion_matrix(Y_test, Y_pred)
+    
+    print("Confusion matrix :")
+    print(cm)
+    
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    
+    disp.plot()
+    
+    plt.title("Decision tree confusion matrics")
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
