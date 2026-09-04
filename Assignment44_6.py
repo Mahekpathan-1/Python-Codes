3344dfde9bfa4fd229210d44aee33b2b53aabaa4
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-09-04 23:25:38 +0530

    Python ML

---
+++ Assignment44_6.py
@@ -0,0 +1,59 @@
+# Sort the DataFrame by Total marks in descending order.
+
+import pandas as pd
+
+def main():
+    Border = '-'* 40
+    
+    Data = {
+        'Name' : ['Amit', 'Sagar', 'Pooja'],
+        'Math' : [85,90,78],
+        'Science' : [92, 88,80],
+        'English' : [75,85,82]
+    }
+    
+    df = pd.DataFrame(Data)
+    
+    print("Dataframe")
+    print(df)
+
+    print(Border)
+    print("Shape is :")
+    print(df.shape)
+    print(Border)
+    
+    print("Columns is :")
+    print(df.columns)
+    print(Border)
+    
+    print("datatype is :")
+    print(df.dtypes)   
+    
+    subject_cols = ['Math', 'Science', 'English'] 
+    
+    df['Total'] = df[subject_cols].sum(axis=1)
+    
+    print(Border)
+    print("Full dataframe with total marks :")
+    print(df)
+    print(Border)
+    
+    science_top = df[df['Science'] > 85 ]
+    
+    print("Students with science marks > 85 :\n", science_top)
+    
+    df['Name'] = df['Name'].replace('Pooja', 'Puja')
+    
+    print(Border)
+    print("Replace Pooja with puja")
+    print(df)
+    
+    sorted_df = df.sort_values(by = 'Total', ascending= False)
+    
+    print(Border)
+    print("Total Marks in descending order :")
+    print(sorted_df)
+
+
+if __name__ == "__main__":
+    main()
