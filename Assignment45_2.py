218f0dee0e765bb229ad4063a8bb510a3c5cfe4c
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-09-04 23:27:59 +0530

    Python ML

---
+++ Assignment45_2.py
@@ -0,0 +1,38 @@
+# Normalize the Math scores using Min-Max Scaling
+
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+
+def main():
+    Border = '-'* 40
+    
+    Data = {
+        'Name' : ['Amit', 'Sagar', 'Pooja'],
+        'Math' : [85,90,78],
+        'Science' : [92, 88,80],
+        'English' : [75,85,82]
+    }
+    
+    df = pd.DataFrame(Data)
+    
+    print(df)
+    print(Border)
+    
+    scalar = MinMaxScaler()
+    
+    print("Using MinMAxscaler :")
+    
+    df['Math'] = scalar.fit_transform(df[['Math']])     # normalize numerical value between 0 and 1
+    
+    print(df)
+    
+    print(Border)
+    
+    df['Gender']= ["Male", "Male","Female"]
+    
+    df = pd.get_dummies(df, columns=["Gender"])
+    
+    print(df)
+    
+if __name__ == "__main__":
+    main()
