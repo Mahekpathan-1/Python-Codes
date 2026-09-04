eee39e740e859f3d00501113c7ee359dad1300df
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-09-04 23:24:43 +0530

    Python ML

---
+++ Assignment44_3.py
@@ -0,0 +1,40 @@
+# add new column 'Total' as some of all the subject marks  
+
+import pandas as pd
+
+def main():
+    Border = '-'* 40
+    
+    Data = {
+        'Name' : ['Amit', 'Sagar', 'Pooja'],
+        'Math' : [85,90,78],
+        'Science' : [92, 88,80],
+        'English' : [75,85,82]
+    }
+    
+    df = pd.DataFrame(Data)
+    
+    print("Dataframe")
+    print(df)
+
+    print(Border)
+    print("Shape is :")
+    print(df.shape)
+    print(Border)
+    
+    print("Columns is :")
+    print(df.columns)
+    print(Border)
+    
+    print("datatype is :")
+    print(df.dtypes)   
+    
+    subject_cols = ['Math', 'Science', 'English'] 
+    
+    df['Total'] = df[subject_cols].sum(axis=1)
+    
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
