7facecb3d13b681bd9528bed52087e2f1672da6b
Mahekpathan-1 <pathanmahek0923@gmail.com>
2026-09-04 23:28:17 +0530

    Python ML

---
+++ Assignment45_3.py
@@ -0,0 +1,40 @@
+# Group students by Gender and calculate average marks
+
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+
+def main():
+    Border = '-'* 40
+    
+    Data = {
+        'Name' : ['Amit', 'Sagar', 'Pooja'],
+        'Math' : [85,90,78],
+        'Science' : [92, 88,80],
+        'English' : [75,85,82]
+    }
+    
+    df = pd.DataFrame(Data)
+    
+    print(df)
+    print(Border)
+    
+    scalar = MinMaxScaler()
+    
+    print("Using MinMAxscaler :")
+    
+    df['Math'] = scalar.fit_transform(df[['Math']])     # normalize numerical value between 0 and 1
+    
+    print(df)
+    
+    print(Border)
+    
+    df['Gender']= ["Male", "Male","Female"]
+    
+    average_marks = df.groupby('Gender')[
+        ['Math','English','Science']
+    ].mean()
+    
+    print(average_marks)
+    
+if __name__ == "__main__":
+    main()
